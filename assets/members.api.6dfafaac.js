f39c762bd3b964afc8c7dce86964a6af4af74ee3
joshtune <joshua.tune@gmail.com>
2022-09-13 08:05:46 -0600

    auto deployment updates

---
+++ assets/members.api.6dfafaac.js
@@ -0,0 +1,1 @@
+import{S as e}from"./supabase.crud.63cff24f.js";class s extends e{constructor(){super("members")}}const o=new s;export{o as m};
--- assets/organizations.api.5cec54c4.js
+++
@@ -1,1 +0,0 @@
-import{S as o}from"./supabase.crud.8aed7da0.js";class s extends o{constructor(){super("organizations")}}const r=new s;export{r as o};
